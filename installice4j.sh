16f2fbeb6a4569cde059ea1779b55dc6b4f3ee37
Tim Panton <thp@westhawk.co.uk>
2015-11-30 12:00:40 +0000

    Add script to install a local ice4j version

---
+++ installice4j.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+JAVA_HOME=/usr '/Applications/NetBeans/NetBeans 8.0.app/Contents/Resources/NetBeans/java/maven/bin/mvn' install:install-file -Dfile=/project2/jitsi/ice4j-read-only/ice4j.jar -DgroupId=org \
+    -DartifactId=ice4j -Dversion=5.09 -Dpackaging=jar
+
